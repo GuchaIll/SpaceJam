c71aef8dfac2e241416d350ad11e3b6a14f32762
GuchaIll <102880287+GuchaIll@users.noreply.github.com>
2023-10-24 22:26:24 +0000

    added web app

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '/static')
 
 @app.route("/")
 def hello():
-  return "Hello World"
-
+  return render_template('home.html')
 print(__name__)
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug = True)
+  
+
+@app.route('Generate', methods=['POST'])
+def Generate():
+    # Your code to perform some action when the button is clicked
+    print("called")
+    return 'Action successful'  # You can return a response, redirect, or any suitable output
